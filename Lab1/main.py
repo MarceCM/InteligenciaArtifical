bef1a20120b4a87863a255d9b9ec615cb2641902
Marcella Costa <48464412+MarceCM@users.noreply.github.com>
2022-08-16 20:14:58 -0300

    Crate other files and start coding

---
+++ Lab1/main.py
@@ -0,0 +1,25 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+myChatBot.loadModel()
+
+#criar o modelo
+myChatBot.createModel()
+
+
+
+
+print("Bem vindo ao MVP da FEI")
+
+
+pergunta = input("O que desja saber sobre nossas relações internacionais?")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("Posso lhe ajudar com algo a mais?")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
