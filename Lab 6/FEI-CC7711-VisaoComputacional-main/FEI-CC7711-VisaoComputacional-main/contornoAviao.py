45303091f97e08e8e7a1a19a901d03cf58dfa3aa
Marcella Costa <marcellacosta368@icloud.com>
2022-09-25 20:43:03 -0300

    updateCodes

--- Lab 6/FEI-CC7711-VisaoComputacional-main/FEI-CC7711-VisaoComputacional-main/contornoAviao.py
+++ Lab 6/FEI-CC7711-VisaoComputacional-main/FEI-CC7711-VisaoComputacional-main/contornoAviao.py
@@ -5,7 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 #Importa e converta para RGB
-img = cv2.imread('./imagens/CASTELO_01.jpeg')
+img = cv2.imread('./imagens/AVIAO_01.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
@@ -13,26 +13,26 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #Convertendo para preto e branco (RGB -> Gray Scale -> BW)
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 a = img_gray.max()
-_, thresh = cv2.threshold(img_gray, a/2*1.1, a,cv2.THRESH_BINARY_INV)
+#_, thresh = cv2.threshold(img_gray, a/2*1.1, a,cv2.THRESH_BINARY_INV)   
 
 
 tamanhoKernel = 5
 kernel = np.ones((tamanhoKernel,tamanhoKernel), np.uint8)
-thresh_open = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+#thresh_open = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
 #Filtro de ruído (bluring)
-img_blur = cv2.blur(img_gray, ksize=(tamanhoKernel,tamanhoKernel))
+#img_blur = cv2.blur(img_gray, ksize=(tamanhoKernel,tamanhoKernel))
 
 # Detecção borda com Canny (sem blurry)
 edges_gray = cv2.Canny(image=img_gray, threshold1=a/2, threshold2=a/2)
 # Detecção borda com Canny (com blurry)
-edges_blur = cv2.Canny(image=img_blur, threshold1=a/2, threshold2=a/2)
+#edges_blur = cv2.Canny(image=img_blur, threshold1=a/2, threshold2=a/2)
 
 
 
 # contorno
 contours, hierarchy = cv2.findContours(
-                                   image = thresh,
+                                   image = edges_gray,
                                    mode = cv2.RETR_TREE,
                                    method = cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours, key = cv2.contourArea, reverse = True)
@@ -42,7 +42,7 @@ final = cv2.drawContours(img_copy, contours, contourIdx = -1,
 
 
 #plot imagens
-imagens = [img,img_blur,img_gray,edges_gray,edges_blur,thresh,thresh_open,final]
+imagens = [img,img_gray,edges_gray,final]
 formatoX = math.ceil(len(imagens)**.5)
 if (formatoX**2-len(imagens))>formatoX:
     formatoY = formatoX-1
