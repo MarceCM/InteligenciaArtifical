2ce70f30ac8c3424d0347d60c2bed23c0e86d6e4
Marcella Costa <marcellacosta368@icloud.com>
2022-10-04 22:16:51 -0300

    updatecode

--- Lab7/obesidade.py
+++ Lab7/obesidade.py
@@ -5,38 +5,34 @@ from skfuzzy import control as ctrl
 
 #Variaveis de Entrada (Antecedent)
 comida = ctrl.Antecedent(np.arange(0, 9, 1), 'comida')
-peso = ctrl.Antecedent(np.arange(0, 11, 1), 'peso')
 
 #Variaveis de saída (Consequent)
-quilo = ctrl.Consequent(np.arange(0, 31, 1), 'quilo')
+quilo = ctrl.Consequent(np.arange(0, 11, 1), 'quilo')
 
 # automf -> Atribuição de categorias automaticamente
 comida.automf(names=['pouco','razoavel','bastante'],)
-peso.automf(names=['leve','medio','pesado'])
 
 # atribuicao gaussiana
-quilo['minimo'] = fuzz.gaussmf(quilo.universe, 0,.1)
-quilo['baixo'] = fuzz.gaussmf(quilo.universe, .1, 3)
-quilo['medio'] = fuzz.gaussmf(quilo.universe, 15,5)
-quilo['alto'] = fuzz.gaussmf(quilo.universe, 30,5)
+quilo['leve'] = fuzz.gaussmf(quilo.universe, 0,.1)
+quilo['medio'] = fuzz.gaussmf(quilo.universe, .1, 3)
+quilo['pesado'] = fuzz.gaussmf(quilo.universe, 15,5)
+
 quilo.view()
 
 # atribuicao trapezoidal
-quilo['minimo'] = fuzz.trapmf(quilo.universe, [0,.1,2,5])
-quilo['baixo'] = fuzz.trapmf(quilo.universe, [.1, 3, 6, 10])
-quilo['medio'] = fuzz.trapmf(quilo.universe, [15,20,25,30])
-quilo['alto'] = fuzz.trapmf(quilo.universe, [30,35,40,45])
+quilo['leve'] = fuzz.trapmf(quilo.universe, [0,0,4,6])
+quilo['medio'] = fuzz.trapmf(quilo.universe, [4, 6, 8, 10])
+quilo['pesado'] = fuzz.trapmf(quilo.universe, [8,10,12,14])
 quilo.view()
 
 #Visualizando as variáveis
 comida.view()
-peso.view()
 
 
 #Criando as regras
-regra_1 = ctrl.Rule(comida['pouco'] & peso['leve'], quilo['minimo'])
-regra_2 = ctrl.Rule(comida['pouco'] & peso['medio'], quilo['medio'])
-regra_3 = ctrl.Rule(peso['pesado'] & comida['bastante'], quilo['alto'])
+regra_1 = ctrl.Rule(comida['pouco'], quilo['leve'])
+regra_2 = ctrl.Rule(comida['razoavel'], quilo['medio'])
+regra_3 = ctrl.Rule(comida['bastante'], quilo['pesado'])
 
 controlador = ctrl.ControlSystem([regra_1, regra_2, regra_3])
 
@@ -44,22 +40,18 @@ controlador = ctrl.ControlSystem([regra_1, regra_2, regra_3])
 #Simulando
 Calculoquilo = ctrl.ControlSystemSimulation(controlador)
 
-notaQualidade = int(input('Comida: '))
-notaServico = int(input('Peso: '))
+notaQualidade = float(input('Comida: '))
 Calculoquilo.input['comida'] = notaQualidade
-Calculoquilo.input['peso'] = notaServico
 Calculoquilo.compute()
 
 valorquilo = Calculoquilo.output['quilo']
 
-print("\nComida %d \nPeso %d \nQuilo %5.2f" %(
+print("\nComida %d \nQuilo %5.2f" %(
         notaQualidade,
-        notaServico,
         valorquilo))
 
 
 comida.view(sim=Calculoquilo)
-peso.view(sim=Calculoquilo)
 quilo.view(sim=Calculoquilo)
 
 plt.show()
